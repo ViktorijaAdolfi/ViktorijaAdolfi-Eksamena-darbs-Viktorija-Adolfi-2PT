af8653973db732c9102003a2c1fb391a19ef5d2c
Viktorija Adolfi <165994061+ViktorijaAdolfi@users.noreply.github.com>
2024-06-13 10:04:45 +0000

    Pielabots pie paša koda un tā izkārtojuma

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ def get_valid_name():
     name = input("Ievadiet savu vārdu: ")
     print()
     if len(name) > 1 and not any(char.isdigit() for char in name):
-        return name
+      return name
     else:
-        print("Nepareizi ievadīts vārds!")
+      print("Nepareizi ievadīts vārds!")
 
 def jaut1():
   print("░░░░░░░░ ░░░░░░░░░░░░░░░░░▀▀▀░░░░░░░░░░░░░░░░")
@@ -37,7 +37,6 @@ def jaut1():
   print("░▀▀▀░▀░░ ░▀▀░░▀░▀░▀▀▀░░▀░░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀")
   print()
   print("Kurš no šiem ir pareizs cikls ar skaitītāju Python valodā, kas izdrukā skaitļus no 1 līdz 5?")
-  # atbildes
   print("  A) for i in range(5): print(i)\n  B) for i in range(1, 5): print(i)\n  C) for i in range(1, 6): print(i - 1)\n  D) for i in range(1, 6): print(i)\n")
 
   while True:
@@ -54,7 +53,6 @@ def jaut2():
   print("░▀▀▀░▀░ ░▀▀░░▀░▀░▀▀▀░░▀░░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀")
   print()
   print("Kurš no šiem ir pareizs cikls ar skaitītāju Python valodā, kas izdrukā skaitļus no 1 līdz 5?")
-  # atbildes
   print("  A) for i in range(5): print(i)\n  B) for i in range(1, 5): print(i)\n  C) for i in range(1, 6): print(i - 1)\n  D) for i in range(1, 6): print(i)\n")
 
   while True:
@@ -66,8 +64,56 @@ def jaut2():
 
 def jaut3():
   print("░░░░░░░ ░░░░░░░░░░░░░░░░░▀▀▀░░░░░░░░░░░░░░░░")
-  print("░▀▀▄░░░ ░▀▀█░█▀█░█░█░▀█▀░█▀█░▀▀█░█░█░█▄█░█▀▀")
-  print("░▄▀░░░░ ░░░█░█▀█░█░█░░█░░█▀█░░░█░█░█░█░█░▀▀█")
+  print("░▀▀█░░░ ░▀▀█░█▀█░█░█░▀█▀░█▀█░▀▀█░█░█░█▄█░█▀▀")
+  print("░░▀▄░░░ ░░░█░█▀█░█░█░░█░░█▀█░░░█░█░█░█░█░▀▀█")
+  print("░▀▀▀░▀░ ░▀▀░░▀░▀░▀▀▀░░▀░░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀")
+  print()
+  print("Kurš no šiem ir pareizs cikls ar skaitītāju Python valodā, kas izdrukā skaitļus no 1 līdz 5?")
+  print("  A) for i in range(5): print(i)\n  B) for i in range(1, 5): print(i)\n  C) for i in range(1, 6): print(i - 1)\n  D) for i in range(1, 6): print(i)\n")
+
+  while True:
+    answer = input("Ievadiet savu atbildi: ").upper()
+    if answer in ('A', 'B', 'C', 'D'):
+      return answer
+    else:
+      print("Nepareizi ievadīts burts!")
+
+def jaut4():
+  print("░░░░░░░ ░░░░░░░░░░░░░░░░░▀▀▀░░░░░░░░░░░░░░░░")
+  print("░█░█░░░ ░▀▀█░█▀█░█░█░▀█▀░█▀█░▀▀█░█░█░█▄█░█▀▀")
+  print("░░▀█░░░ ░░░█░█▀█░█░█░░█░░█▀█░░░█░█░█░█░█░▀▀█")
+  print("░░░▀░▀░ ░▀▀░░▀░▀░▀▀▀░░▀░░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀")
+  print()
+  print("Kurš no šiem ir pareizs cikls ar skaitītāju Python valodā, kas izdrukā skaitļus no 1 līdz 5?")
+  print("  A) for i in range(5): print(i)\n  B) for i in range(1, 5): print(i)\n  C) for i in range(1, 6): print(i - 1)\n  D) for i in range(1, 6): print(i)\n")
+
+  while True:
+    answer = input("Ievadiet savu atbildi: ").upper()
+    if answer in ('A', 'B', 'C', 'D'):
+      return answer
+    else:
+      print("Nepareizi ievadīts burts!")
+
+def jaut5():
+  print("░░░░░░░ ░░░░░░░░░░░░░░░░░▀▀▀░░░░░░░░░░░░░░░░")
+  print("░█▀▀░░░ ░▀▀█░█▀█░█░█░▀█▀░█▀█░▀▀█░█░█░█▄█░█▀▀")
+  print("░▀▀▄░░░ ░░░█░█▀█░█░█░░█░░█▀█░░░█░█░█░█░█░▀▀█")
+  print("░▀▀░░▀░ ░▀▀░░▀░▀░▀▀▀░░▀░░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀")
+  print()
+  print("Kurš no šiem ir pareizs cikls ar skaitītāju Python valodā, kas izdrukā skaitļus no 1 līdz 5?")
+  print("  A) for i in range(5): print(i)\n  B) for i in range(1, 5): print(i)\n  C) for i in range(1, 6): print(i - 1)\n  D) for i in range(1, 6): print(i)\n")
+
+  while True:
+    answer = input("Ievadiet savu atbildi: ").upper()
+    if answer in ('A', 'B', 'C', 'D'):
+      return answer
+    else:
+      print("Nepareizi ievadīts burts!")
+
+def jaut6():
+  print("░░░░░░░ ░░░░░░░░░░░░░░░░░▀▀▀░░░░░░░░░░░░░░░░")
+  print("░▄▀▀░░░ ░▀▀█░█▀█░█░█░▀█▀░█▀█░▀▀█░█░█░█▄█░█▀▀")
+  print("░█▀▄░░░ ░░░█░█▀█░█░█░░█░░█▀█░░░█░█░█░█░█░▀▀█")
   print("░▀▀▀░▀░ ░▀▀░░▀░▀░▀▀▀░░▀░░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀")
   print()
   print("Kurš no šiem ir pareizs cikls ar skaitītāju Python valodā, kas izdrukā skaitļus no 1 līdz 5?")
@@ -80,21 +126,88 @@ def jaut3():
       return answer
     else:
       print("Nepareizi ievadīts burts!")
-      
-def checkAnswers(answers):
-  answerText ="";
-  points=0;
-  rightans = ['D','A','A','B','C','A','D','B','A','B']
-  for i in range(len(answers)):
-    if(rightans[i]==rightans[i]):
-      points=points+1
 
-    if(rightans[i]==rightans[i]):
-      answerText=answerText+"\nPareizi\n1. jautājums: \nJūsu atbilde: "+str(answers[i])+"\nPareizā atbilde: "+str(rightans[i])
-    elif(rightans[i]!=rightans[i]):
-      answerText=answerText+"\nNepareizi\n1. jautājums: \nJūsu atbilde: "+str(answers[i])+"\nPareizā atbilde: "+str(rightans[i])
-  
+def jaut7():
+  print("░░░░░░░ ░░░░░░░░░░░░░░░░░▀▀▀░░░░░░░░░░░░░░░░")
+  print("░▀▀█░░░ ░▀▀█░█▀█░█░█░▀█▀░█▀█░▀▀█░█░█░█▄█░█▀▀")
+  print("░▄▀░░░░ ░░░█░█▀█░█░█░░█░░█▀█░░░█░█░█░█░█░▀▀█")
+  print("░▀░░░▀░ ░▀▀░░▀░▀░▀▀▀░░▀░░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀")
+  print()
+  print("Kurš no šiem ir pareizs cikls ar skaitītāju Python valodā, kas izdrukā skaitļus no 1 līdz 5?")
+  print("  A) for i in range(5): print(i)\n  B) for i in range(1, 5): print(i)\n  C) for i in range(1, 6): print(i - 1)\n  D) for i in range(1, 6): print(i)\n")
+
+  while True:
+    answer = input("Ievadiet savu atbildi: ").upper()
+    if answer in ('A', 'B', 'C', 'D'):
+      return answer
+    else:
+      print("Nepareizi ievadīts burts!")
+
+def jaut8():
+  print("░░░░░░░ ░░░░░░░░░░░░░░░░░▀▀▀░░░░░░░░░░░░░░░░")
+  print("░▄▀▄░░░ ░▀▀█░█▀█░█░█░▀█▀░█▀█░▀▀█░█░█░█▄█░█▀▀")
+  print("░▄▀▄░░░ ░░░█░█▀█░█░█░░█░░█▀█░░░█░█░█░█░█░▀▀█")
+  print("░░▀░░▀░ ░▀▀░░▀░▀░▀▀▀░░▀░░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀")
+  print()
+  print("Kurš no šiem ir pareizs cikls ar skaitītāju Python valodā, kas izdrukā skaitļus no 1 līdz 5?")
+  print("  A) for i in range(5): print(i)\n  B) for i in range(1, 5): print(i)\n  C) for i in range(1, 6): print(i - 1)\n  D) for i in range(1, 6): print(i)\n")
+
+  while True:
+    answer = input("Ievadiet savu atbildi: ").upper()
+    if answer in ('A', 'B', 'C', 'D'):
+      return answer
+    else:
+      print("Nepareizi ievadīts burts!")
+
+def jaut9():
+  print("░░░░░░░ ░░░░░░░░░░░░░░░░░▀▀▀░░░░░░░░░░░░░░░░")
+  print("░▄▀▄░░░ ░▀▀█░█▀█░█░█░▀█▀░█▀█░▀▀█░█░█░█▄█░█▀▀")
+  print("░░▀█░░░ ░░░█░█▀█░█░█░░█░░█▀█░░░█░█░█░█░█░▀▀█")
+  print("░▀▀░░▀░ ░▀▀░░▀░▀░▀▀▀░░▀░░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀")
+  print()
+  print("Kurš no šiem ir pareizs cikls ar skaitītāju Python valodā, kas izdrukā skaitļus no 1 līdz 5?")
+  print("  A) for i in range(5): print(i)\n  B) for i in range(1, 5): print(i)\n  C) for i in range(1, 6): print(i - 1)\n  D) for i in range(1, 6): print(i)\n")
+
+  while True:
+    answer = input("Ievadiet savu atbildi: ").upper()
+    if answer in ('A', 'B', 'C', 'D'):
+      return answer
+    else:
+      print("Nepareizi ievadīts burts!")
+      
+def jaut10():
+  print("░░░░░░░░░░░ ░░░░░░░░░░░░░░░░░▀▀▀░░░░░░░░░░░░░░░░")
+  print("░▀█░░▄▀▄░░░ ░▀▀█░█▀█░█░█░▀█▀░█▀█░▀▀█░█░█░█▄█░█▀▀")
+  print("░░█░░█/█░░░ ░░░█░█▀█░█░█░░█░░█▀█░░░█░█░█░█░█░▀▀█")
+  print("░▀▀▀░░▀░░▀░ ░▀▀░░▀░▀░▀▀▀░░▀░░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀")
+  print()
+  print("Kurš no šiem ir pareizs cikls ar skaitītāju Python valodā, kas izdrukā skaitļus no 1 līdz 5?")
+  print("  A) for i in range(5): print(i)\n  B) for i in range(1, 5): print(i)\n  C) for i in range(1, 6): print(i - 1)\n  D) for i in range(1, 6): print(i)\n")
+
+  while True:
+    answer = input("Ievadiet savu atbildi: ").upper()
+    if answer in ('A', 'B', 'C', 'D'):
+      return answer
+    else:
+      print("Nepareizi ievadīts burts!")
+
+def checkAnswers(answers):
+  answerText = ""
+  points = 0
+  rightans = ['D', 'A', 'A', 'B', 'C', 'A', 'D', 'B', 'A', 'B']
+  for i in range(len(answers)):
+    if (rightans[i] == rightans[i]):
+      points = points + 1
+
+    if (rightans[i] == rightans[i]):
+      answerText = answerText + "\nPareizi\n1. jautājums: \nJūsu atbilde: " + str(
+          answers[i]) + "\nPareizā atbilde: " + str(rightans[i])
+    elif (rightans[i] != rightans[i]):
+      answerText = answerText + "\nNepareizi\n1. jautājums: \nJūsu atbilde: " + str(
+          answers[i]) + "\nPareizā atbilde: " + str(rightans[i])
+
   return answerText
+
 
 punkti = 0
 answers = []
@@ -102,10 +215,9 @@ rightans = []
 hello()
 noteikumi()
 username = get_valid_name()
-print("Esi sveicināts "+username+"! \nLai tev veicas testā!")
+print("Esi sveicināts " + username + "! \nLai tev veicas testā!")
 clear5S()
 answers.append(jaut1())
 answers.append(jaut2())
 # checkAnswers(answers)
 print(checkAnswers(answers))
-
